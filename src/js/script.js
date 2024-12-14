0a9f61437c643f8ccd258d5d0e0a7d5f3a6f021d
ricardo <rihozmar@gmail.com>
2024-12-14 21:46:26 +0100

    Header y estructura básica del script.js

--- src/js/script.js
+++ src/js/script.js
@@ -1,33 +1,49 @@
 //Apis keys
 const ticketMasterApiKey="T6MA2HQ4qpcEsWlYWKr8MR3Ux9mDOMQ4"
-//Variables
-let artista=document.getElementById("artista")
-let enviar=document.getElementById("enviar")
-//Para recogerlo por id la imagen
+//Vars
+let llamarModal=document.getElementById("llamarModal")
+//Take images by id
 // const api=await fetch("https://app.ticketmaster.com/discovery/v2/events/Z698xZ8KZ17uko9/images?apikey="+ticketMasterApiKey)
 
-//Funciones
+//Function
 
-const peticionApiTicketMaster=async(nombreArtista)=>{
+//Funcion para recoger la información de los eventos que dará un artista introducido por parametro.
+const peticionApiTicketMasterArtista=async(nombreArtista)=>{
     //Para recogerlo por nombre
     const api=await fetch(`https://app.ticketmaster.com/discovery/v2/events?keyword=${nombreArtista}&apikey=`+ticketMasterApiKey)
     const data=await api.json()
     return data
 }
+//Funcion que nos devuelve información de la api de Itunes.
 const peticionItunes=async(nombreArtista)=>{
     const api=await fetch(" https://itunes.apple.com/search?term="+nombreArtista)
     const data =await api.json()
     return data
 }
-const listar=async()=>{
-    let nombreArtista=artista.value
-    const info=await peticionApiTicketMaster(nombreArtista)
-    console.log(info._embedded.events)
-    const itunes=await peticionItunes(info._embedded.events[0].name)
-    console.log(itunes)
+
+//Funcion para recoger la información de los eventos que se van a dar en Madrid de la segunda página.
+const peticionApiTicketMasterGeneral=async()=>{
+    const api=await fetch(`https://app.ticketmaster.com/discovery/v2/events?city=Madrid&page=2&apikey=`+ticketMasterApiKey)
+    const data=await api.json()
+    return data
+}
+//Función para mostrar los datos por pantalla de la api
+const listar=async(event)=>{
+    const cargaIndex=await peticionApiTicketMasterGeneral()
+    console.log(cargaIndex)
+    if(event.target.nodeName=="BUTTON"){
+        let nombreArtista=artista.value
+        const info=await peticionApiTicketMasterArtista(nombreArtista)
+        console.log(info._embedded.events)
+        const itunes=await peticionItunes(info._embedded.events[0].name)
+        console.log(itunes)
+    }
+}
+//Función para hacer aparecer la ventana modal
+const ventanaModal=(event)=>{
+
 }
 
-
-//Listener
-enviar.addEventListener("click",listar)
-enviar.addEventListener("DOMContentLoaded",listar)
+//Listeners
+document.addEventListener("DOMContentLoaded",listar)
+llamarModal.addEventListener("click",ventanaModal)
