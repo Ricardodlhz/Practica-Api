2fd0cfda5caf892c8bb284b4f3cbde5af175d42c
ricardo <rihozmar@gmail.com>
2024-12-19 12:22:32 +0100

    Carga de EVentos deportivos y cambio en el header para hacerlo Fixed

--- src/js/script.js
+++ src/js/script.js
@@ -2,7 +2,7 @@
 const ticketMasterApiKey="T6MA2HQ4qpcEsWlYWKr8MR3Ux9mDOMQ4"
 //Vars
 let llamarModal=document.getElementById("llamarModal")
-let main=document.getElementById("main")
+let main=document.getElementById("containerConciertos")
 let img=document.getElementById("img")
 //Take images by id
 // const api=await fetch("https://app.ticketmaster.com/discovery/v2/events/Z698xZ8KZ17uko9/images?apikey="+ticketMasterApiKey)
@@ -29,19 +29,17 @@ const peticionApiTicketMasterGeneral=async()=>{
     const data=await api.json()
     return data
 }
-//Función para mostrar los datos por pantalla de la api
-let fragment=document.createDocumentFragment()
-const listarEventoArtista=async(event)=>{
-    if(event.target.nodeName=="BUTTON"){
-        let nombreArtista=artista.value
-        const info=await peticionApiTicketMasterArtista(nombreArtista)
-        console.log(info._embedded.events)
-        const itunes=await peticionItunes(info._embedded.events[0].name)
-        console.log(itunes)
-    }
+//Función para Recoger la informacion de los eventos deportivos
+const peticionApiTicketMasterDeportes=async()=>{
+    const api=await fetch(`https://app.ticketmaster.com/discovery/v2/events.json?segmentId=KZFzniwnSyZfZ7v7nE&apikey=`+ticketMasterApiKey)
+    const data=await api.json()
+    return data
 }
 
-//Función para crear los elementos del index
+
+
+//Función para crear los elementos con la información de la api
+let fragment=document.createDocumentFragment()
 const crearElementos=(evento)=>{
     let newArticle=document.createElement("ARTICLE")
     let newTitulo=document.createElement("H2")
@@ -50,8 +48,11 @@ const crearElementos=(evento)=>{
     let newP=document.createElement("P")
     newArticle.classList.add("cards")
     newImg.classList.add("imagen")
+    newTitulo.classList.add("titulo")
+    newA.classList.add("texto")
+    newP.classList.add("texto")
     newTitulo.textContent=evento.name
-    newImg.src=evento.images[2].url
+    newImg.src=evento.images[4].url
     newA.textContent="Comprar Entradas"
     newA.href=evento.url
     newA.target="_blank"
@@ -63,16 +64,35 @@ const crearElementos=(evento)=>{
     fragment.appendChild(newArticle)
     main.appendChild(fragment)
 }
-//Función para cargar los eventos de Madrid en el index.html
+//Función para cargar los eventos en el index.html
 const listarEventosIndex=async()=>{
     
     const cargaIndex=await peticionApiTicketMasterGeneral()
     const arrayEventos=cargaIndex._embedded.events
-    arrayEventos.forEach(evento => {
-       crearElementos(evento)
-    })
+    const cargarDeportes=await peticionApiTicketMasterDeportes()
+    const arrayEventosDeportes=cargarDeportes._embedded.events
+    //Mostramos los Eventos de Musica en Madrid
+    for (let evento = 0; evento < 5; evento++) {
+       crearElementos(arrayEventos[evento])
+    }
     
-    
+    //  console.log(cargaIndex)
+    console.log(cargarDeportes)
+    //Mostramos los Eventos de Deportes
+    for (let evento = 0; evento < 5; evento++) {
+        crearElementos(arrayEventosDeportes[evento])
+     }
+}
+
+//Función para mostrar los datos por pantalla de la api de un artisto en concreto
+const listarEventoArtista=async(event)=>{
+    if(event.target.nodeName=="BUTTON"){
+        let nombreArtista=artista.value
+        const info=await peticionApiTicketMasterArtista(nombreArtista)
+        console.log(info._embedded.events)
+        const itunes=await peticionItunes(info._embedded.events[0].name)
+        console.log(itunes)
+    }
 }
 //Función para hacer aparecer la ventana modal
 const ventanaModal=(event)=>{
